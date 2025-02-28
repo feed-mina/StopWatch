bc0bf9ef45d68d3c794ead80172d130f6765ccff
feed-mina <myelin24@naver.com>
2025-02-28 18:29:03 +0900

    스탑워치 & 뽀모도로 App.vue 수정

--- tutorialApp/MyVueApp/my-pwa-app/dev-dist/sw.js
+++ tutorialApp/MyVueApp/my-pwa-app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ab4816ec'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b469o0ui4u"
+    "revision": "0.mm3e96l8v88"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- tutorialApp/MyVueApp/my-pwa-app/src/App.vue
+++ tutorialApp/MyVueApp/my-pwa-app/src/App.vue
@@ -1,18 +1,78 @@
 <template>
-  <div>
-    <h1>PWA 적용 완료!</h1>
-    <p>이제 이 웹사이트는 오프라인에서도 실행할 수 있어요.</p>
+  <div class="app">
+    <h1>🕒 스탑워치 & 뽀모도로</h1>
+    
+    <div class="timer">
+      <h2>{{ formattedTime }}</h2>
+      <button @click="startTimer" v-if="!running">시작</button>
+      <button @click="stopTimer" v-if="running">정지</button>
+      <button @click="resetTimer">초기화</button>
+    </div>
+
+    <div class="pomo">
+      <h3>뽀모도로 세션: {{ pomoSession }}</h3>
+      <button @click="startPomodoro">뽀모도로 시작</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  data() {
+    return {
+      time: 0,
+      running: false,
+      pomoSession: 0,
+      interval: null
+    };
+  },
+  computed: {
+    formattedTime() {
+      let min = Math.floor(this.time / 60);
+      let sec = this.time % 60;
+      return `${min}:${sec < 10 ? '0' : ''}${sec}`;
+    }
+  },
+  methods: {
+    startTimer() {
+      this.running = true;
+      this.interval = setInterval(() => {
+        this.time++;
+      }, 1000);
+    },
+    stopTimer() {
+      this.running = false;
+      clearInterval(this.interval);
+    },
+    resetTimer() {
+      this.running = false;
+      clearInterval(this.interval);
+      this.time = 0;
+    },
+    startPomodoro() {
+      this.resetTimer();
+      this.pomoSession++;
+      this.startTimer();
+      setTimeout(() => {
+        this.stopTimer();
+        alert("뽀모도로 세션 종료!");
+      }, 25 * 60 * 1000); // 25분 타이머
+    }
+  }
+};
 </script>
 
 <style>
-h1 {
-  color: #42b983;
+.app {
+  text-align: center;
+  margin-top: 50px;
+}
+.timer h2 {
+  font-size: 48px;
+}
+button {
+  margin: 10px;
+  padding: 10px 20px;
+  font-size: 16px;
 }
 </style>
