8c56c1c978c92de6d9d6ea44f91b006759514532
feed-mina <myelin24@naver.com>
2025-03-03 14:38:18 +0900

    electron 창 관련 main.cjs 설정 수정

--- tutorialApp/MyVueApp/my-pwa-app/electron/main.cjs
+++ tutorialApp/MyVueApp/my-pwa-app/electron/main.cjs
@@ -1,45 +1,28 @@
 const { app, BrowserWindow, session } = require('electron');
-const { autoUpdater } = require('electron-updater');
-
-const path = require('path')
 
 let mainWindow;
 
 app.whenReady().then(() => {
   mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-          nodeIntegration: true,
-          contextIsolation: false,
-          webSecurity: false  // 보안주의
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,  // Node.js를 같이 사용하게 해요.
+      contextIsolation: false // 격리하지 않고 같이 돌아가게 해요.
+    }
+  });
+
+  // 🛡️ Electron 창에 보안 규칙 넣기
+  mainWindow.webContents.session.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        "Content-Security-Policy": [
+          "default-src 'self'; script-src 'self' 'unsafe-inline' https://developers.kakao.com https://t1.kakaocdn.net https://accounts.kakao.com; style-src 'self' 'unsafe-inline' https://accounts.kakao.com https://t1.kakaocdn.net; connect-src 'self' http://localhost:8080 https://kauth.kakao.com https://accounts.kakao.com; frame-src https://kauth.kakao.com https://accounts.kakao.com;"
+        ]
       }
+    });
   });
 
   mainWindow.loadURL('http://localhost:5173');
-
-    //  devtools 관련 예외처리 추가 열지 못하게 막기
-    // try {
-    //     session.defaultSession.webRequest.onBeforeRequest((details, callback) => {
-    //         if (details.url.startsWith("devtools://")) {
-    //             return callback({ cancel: true }); // DevTools 차단
-    //         }
-    //         return callback({});
-    //     });
-    // } catch (error) {
-    //     console.error("onBeforeRequest Error:", error);
-    // }
-
-    //  Electron 실행 중 Promise 오류 방지
-    mainWindow.webContents.session.webRequest.onHeadersReceived((details, callback) => {
-      callback({
-        responseHeaders: {
-          ...details.responseHeaders,
-          "Content-Security-Policy": [
-            "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://developers.kakao.com https://t1.kakaocdn.net https://accounts.kakao.com; style-src 'self' 'unsafe-inline' https://accounts.kakao.com https://t1.kakaocdn.net; connect-src 'self' http://localhost:8080 https://kauth.kakao.com https://accounts.kakao.com; frame-src https://kauth.kakao.com https://accounts.kakao.com;"
-          ]
-        }
-      });
-    });
-    
 });
